0644ef91be2be809b72498a1402a8795648ee914
Vladimir Abramyants <abramyants@aurum.org>
2019-10-09 17:02:23 +0300

    Create models

--- app/app.py
+++ app/app.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 from config import Configuration
 from posts.blueprint import posts
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+db = SQLAlchemy(app)
 
 app.register_blueprint(posts, url_prefix='/blog')
--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
 class Configuration:
 
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_DB_URI = 'mysql+mysqlconnector://root@localhost/test_blog'
